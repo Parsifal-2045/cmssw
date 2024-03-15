db8a7331c9d3ea52e6701cee2f62c04af594aa31
Carlo Battilana <carlo.battilana@gmail.com>
2024-03-15 18:33:05 +0100

    Extend NANO mu HLT ntuple content

--- DPGAnalysis/MuonTools/python/nano_phase2_muon_trig.py
+++ DPGAnalysis/MuonTools/python/nano_phase2_muon_trig.py
@@ -12,45 +12,173 @@ from Validation.RecoMuon.muonValidationHLT_cff import *
 
 l1TkMuTable = cms.EDProducer("SimpleTriggerL1TkMuonFlatTableProducer",
     src = cms.InputTag("l1tTkMuonsGmt"),
-    # minBX = cms.int32(-2),
-    # maxBX = cms.int32(2),                           
     cut = cms.string(""), 
     name= cms.string("L1TkMu"),
     doc = cms.string(""),
     extension = cms.bool(False), 
-    variables = cms.PSet(l1ObjVars,
-                         phPt = Var("phPt()", "float", doc="Physics pt")
-                         # hwCharge = Var("hwCharge()","int16",doc="Charge (can be 0 if the charge measurement was not valid)"),
-                         # hwChargeValid = Var("hwChargeValid()","int16",doc=""),
-                         # tfMuonIndex = Var("tfMuonIndex()","uint16",doc="Index of muon at the uGMT input. 3 indices per link/sector/wedge. EMTF+ are 0-17, OMTF+ are 18-35, BMTF are 36-71, OMTF- are 72-89, EMTF- are 90-107"),
-                         # hwTag = Var("hwTag()","int16",doc="not in L1 ntuples"),
-                         # hwEtaAtVtx = Var("hwEtaAtVtx()","int16",doc="hardware eta estimated at the vertex"),
-                         # hwPhiAtVtx = Var("hwPhiAtVtx()","int16",doc="hardware phi estimated at the vertex"),
-                         # etaAtVtx = Var("etaAtVtx()",float,doc="eta estimated at the vertex"),
-                         # phiAtVtx = Var("phiAtVtx()",float,doc="phi estimated at the vertex"),
-                         # hwIsoSum = Var("hwIsoSum()","int16",doc="not in L1 ntuples"),
-                         # hwDPhiExtra = Var("hwDPhiExtra()","int16",doc="Delta between Pseudo-rapidity at the muon system and the projected coordinate at the vertex in HW unit (for future l1t-integration-tag"),
-                         # hwDEtaExtra = Var("hwDEtaExtra()","int16",doc="Delta between Azimuth at the muon system and the projected coordinate at the vertex in HW unit (for future l1t-integration-tag)"),
-                         # hwRank = Var("hwRank()","int16",doc="not in L1Ntuples"),
-                         # hwPtUnconstrained = Var("hwPtUnconstrained()","int16",doc=""),
-                         # ptUnconstrained = Var("ptUnconstrained()",float,doc=""),
-                         # hwDXY = Var("hwDXY()","uint16",doc=""),
+    variables = cms.PSet(pt = Var("phPt()", "float", doc="Physics pt"),
+                         eta = Var("phEta()", "float", doc="#eta"),
+                         phi = Var("phPhi()", "float", doc="#phi (rad)"),
+                         dXY = Var("phD0()", "float", doc="dXY (cm)"),
+                         dZ = Var("phZ0()", "float", doc="dZ (cm)")
                      )
 )
 
-l2MuTable = cms.EDProducer("SimpleTriggerL2TkMuonFlatTableProducer",
-    src = cms.InputTag("hltL2MuonsFromL1TkMuon"),
+l2SeedTable = cms.EDProducer("SimpleTrajectorySeedFlatTableProducer",
+    src = cms.InputTag("hltL2OfflineMuonSeeds"),
     cut = cms.string(""), 
-    name= cms.string("L2Mu"),
+    name= cms.string("l2_seed"),
     doc = cms.string(""),
     extension = cms.bool(False), 
-    variables = cms.PSet(pt = Var("pt()", "float", doc="Physics pt")
-                     )
+    variables = cms.PSet(pt = Var("startingState().pt()", "float", doc="p_T (GeV)"),
+                         nHits = Var("nHits()", "int16", doc="")
+                         )
 )
 
+l2SeedFromL1TkMuonTable = cms.EDProducer("SimpleTrajectorySeedFlatTableProducer",
+    src = cms.InputTag("hltL2MuonSeedsFromL1TkMuon"),
+    cut = cms.string(""), 
+    name= cms.string("l2_seed_froml1"),
+    doc = cms.string(""),
+    extension = cms.bool(False), 
+    variables = cms.PSet(pt = Var("startingState().pt()", "float", doc="p_T (GeV)"),
+                         nHits = Var("nHits()", "int16", doc="")
+                         )
+)
 
+l2MuTable = cms.EDProducer("SimpleTriggerTrackFlatTableProducer",
+    src = cms.InputTag("hltL2MuonsFromL1TkMuon"),
+    cut = cms.string(""), 
+    name= cms.string("l2_mu"),
+    doc = cms.string(""),
+    extension = cms.bool(False), 
+    variables = cms.PSet(pt = Var("pt()", "float", doc="p_T (GeV)"),
+                         eta = Var("eta()", "float", doc="#eta"),
+                         phi = Var("phi()", "float", doc="#phi (rad)"),
+                         dXY = Var("dxy()", "float", doc="dXY (cm)"),
+                         dZ = Var("dz()", "float", doc="dZ (cm)"),
+                         t0 = Var("t0()", "float", doc="t0 (ns)"),
+                         nPixelHits = Var("hitPattern().numberOfValidPixelHits()", "int16", doc=""),
+                         nTrkLays = Var("hitPattern().trackerLayersWithMeasurement()", "int16", doc=""),
+                         nMuHits = Var("hitPattern().numberOfValidMuonHits()", "int16", doc="")
+                         )
+)
+
+l2MuTableVtx = cms.EDProducer("SimpleTriggerTrackFlatTableProducer",
+    src = cms.InputTag("hltL2MuonsFromL1TkMuon","UpdatedAtVtx"),
+    cut = cms.string(""), 
+    name= cms.string("l2_mu_vtx"),
+    doc = cms.string(""),
+    extension = cms.bool(False), 
+    variables = cms.PSet(pt = Var("pt()", "float", doc="p_T (GeV)"),
+                         eta = Var("eta()", "float", doc="#eta"),
+                         phi = Var("phi()", "float", doc="#phi (rad)"),
+                         dXY = Var("dxy()", "float", doc="dXY (cm)"),
+                         dZ = Var("dz()", "float", doc="dZ (cm)"),
+                         t0 = Var("t0()", "float", doc="t0 (ns)"),
+                         nPixelHits = Var("hitPattern().numberOfValidPixelHits()", "int16", doc=""),
+                         nTrkLays = Var("hitPattern().trackerLayersWithMeasurement()", "int16", doc=""),
+                         nMuHits = Var("hitPattern().numberOfValidMuonHits()", "int16", doc="")
+                         )
+)
+
+l3OIMuTable = cms.EDProducer("SimpleTriggerTrackFlatTableProducer",
+    src = cms.InputTag("hltL3MuonsPhase2L3OI"),
+    cut = cms.string(""), 
+    name= cms.string("l3_mu_OI"),
+    doc = cms.string(""),
+    extension = cms.bool(False), 
+    variables = cms.PSet(pt = Var("pt()", "float", doc="p_T (GeV)"),
+                         eta = Var("eta()", "float", doc="#eta"),
+                         phi = Var("phi()", "float", doc="#phi (rad)"),
+                         dXY = Var("dxy()", "float", doc="dXY (cm)"),
+                         dZ = Var("dz()", "float", doc="dZ (cm)"),
+                         t0 = Var("t0()", "float", doc="t0 (ns)"),
+                         nPixelHits = Var("hitPattern().numberOfValidPixelHits()", "int16", doc=""),
+                         nTrkLays = Var("hitPattern().trackerLayersWithMeasurement()", "int16", doc=""),
+                         nMuHits = Var("hitPattern().numberOfValidMuonHits()", "int16", doc="")
+                         )
+)
+
+l3IOMuTable = cms.EDProducer("SimpleTriggerTrackFlatTableProducer",
+    src = cms.InputTag("hltIter2Phase2L3FromL1TkMuonMerged"),
+    cut = cms.string(""), 
+    name= cms.string("l3_mu_IO"),
+    doc = cms.string(""),
+    extension = cms.bool(False), 
+    variables = cms.PSet(pt = Var("pt()", "float", doc="p_T (GeV)"),
+                         eta = Var("eta()", "float", doc="#eta"),
+                         phi = Var("phi()", "float", doc="#phi (rad)"),
+                         dXY = Var("dxy()", "float", doc="dXY (cm)"),
+                         dZ = Var("dz()", "float", doc="dZ (cm)"),
+                         t0 = Var("t0()", "float", doc="t0 (ns)"),
+                         nPixelHits = Var("hitPattern().numberOfValidPixelHits()", "int16", doc=""),
+                         nTrkLays = Var("hitPattern().trackerLayersWithMeasurement()", "int16", doc=""),
+                         nMuHits = Var("hitPattern().numberOfValidMuonHits()", "int16", doc="")
+                         )
+)
+
+l3MergedMuTable = cms.EDProducer("SimpleTriggerTrackFlatTableProducer",
+    src = cms.InputTag("hltPhase2L3MuonMerged"),
+    cut = cms.string(""), 
+    name= cms.string("l3_mu_merged"),
+    doc = cms.string(""),
+    extension = cms.bool(False), 
+    variables = cms.PSet(pt = Var("pt()", "float", doc="p_T (GeV)"),
+                         eta = Var("eta()", "float", doc="#eta"),
+                         phi = Var("phi()", "float", doc="#phi (rad)"),
+                         dXY = Var("dxy()", "float", doc="dXY (cm)"),
+                         dZ = Var("dz()", "float", doc="dZ (cm)"),
+                         t0 = Var("t0()", "float", doc="t0 (ns)"),
+                         nPixelHits = Var("hitPattern().numberOfValidPixelHits()", "int16", doc=""),
+                         nTrkLays = Var("hitPattern().trackerLayersWithMeasurement()", "int16", doc=""),
+                         nMuHits = Var("hitPattern().numberOfValidMuonHits()", "int16", doc="")
+                        )
+)
+
+l3MuTkIDTable = cms.EDProducer("SimpleTriggerTrackFlatTableProducer",
+    src = cms.InputTag("hltPhase2L3MuonTracks"),
+    cut = cms.string(""), 
+    name= cms.string("l3_mu_ID"),
+    doc = cms.string(""),
+    extension = cms.bool(False), 
+    variables = cms.PSet(pt = Var("pt()", "float", doc="p_T (GeV)"),
+                         eta = Var("eta()", "float", doc="#eta"),
+                         phi = Var("phi()", "float", doc="#phi (rad)"),
+                         dXY = Var("dxy()", "float", doc="dXY (cm)"),
+                         dZ = Var("dz()", "float", doc="dZ (cm)"),
+                         t0 = Var("t0()", "float", doc="t0 (ns)"),
+                         nPixelHits = Var("hitPattern().numberOfValidPixelHits()", "int16", doc=""),
+                         nTrkLays = Var("hitPattern().trackerLayersWithMeasurement()", "int16", doc=""),
+                         nMuHits = Var("hitPattern().numberOfValidMuonHits()", "int16", doc="")
+                         )
+)
+l3OIMuTable = cms.EDProducer("SimpleTriggerTrackFlatTableProducer",
+    src = cms.InputTag("hltL3MuonsPhase2L3OI"),
+    cut = cms.string(""), 
+    name= cms.string("l3_mu_OI"),
+    doc = cms.string(""),
+    extension = cms.bool(False), 
+    variables = cms.PSet(pt = Var("pt()", "float", doc="p_T (GeV)"),
+                         eta = Var("eta()", "float", doc="#eta"),
+                         phi = Var("phi()", "float", doc="#phi (rad)"),
+                         dXY = Var("dxy()", "float", doc="dXY (cm)"),
+                         dZ = Var("dz()", "float", doc="dZ (cm)"),
+                         t0 = Var("t0()", "float", doc="t0 (ns)"),
+                         nPixelHits = Var("hitPattern().numberOfValidPixelHits()", "int16", doc=""),
+                         nTrkLays = Var("hitPattern().trackerLayersWithMeasurement()", "int16", doc=""),
+                         nMuHits = Var("hitPattern().numberOfValidMuonHits()", "int16", doc="")
+                         )
+)
 
 muTriggerProducers = cms.Sequence(l1TkMuTable
+                                  + l2SeedTable
+                                  + l2SeedFromL1TkMuonTable
                                   + l2MuTable
+                                  + l2MuTableVtx
+                                  + l3OIMuTable
+                                  + l3IOMuTable
+                                  + l3MergedMuTable
+                                  + l3MuTkIDTable                                  
                                   + recoMuonValidationHLT_seq
                                 )
