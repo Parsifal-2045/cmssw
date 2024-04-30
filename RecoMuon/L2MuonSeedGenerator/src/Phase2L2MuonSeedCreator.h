c075f90a0cbb790836c0f5526cd60e3bbf283a0a
Luca Ferragina <lferragi@patatrack02.cern.ch>
2024-04-30 18:30:57 +0200

    Modify seed creation logic - Seeds are now built starting from DT / CSC segments - Only DT implemented (untested) - Geometry information used to pass from local to global coordinates depending on the detId

--- RecoMuon/L2MuonSeedGenerator/src/Phase2L2MuonSeedCreator.h
+++ RecoMuon/L2MuonSeedGenerator/src/Phase2L2MuonSeedCreator.h
@@ -7,12 +7,10 @@
  */
 
 #include "DataFormats/MuonSeed/interface/L2MuonTrajectorySeed.h"
-#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
 #include "DataFormats/L1TMuonPhase2/interface/TrackerMuon.h"
-#include "RecoMuon/TransientTrackingRecHit/interface/MuonTransientTrackingRecHit.h"
 
 #include "FWCore/Framework/interface/stream/EDProducer.h"
-#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/Framework/interface/ESHandle.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/ConsumesCollector.h"
 #include "FWCore/Framework/interface/EventSetup.h"
@@ -21,6 +19,21 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/Utilities/interface/ESGetToken.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+
+#include "Geometry/DTGeometry/interface/DTGeometry.h"
+#include "Geometry/CSCGeometry/interface/CSCGeometry.h"
+#include "Geometry/Records/interface/MuonGeometryRecord.h"
+
+#include "RecoMuon/TransientTrackingRecHit/interface/MuonTransientTrackingRecHit.h"
+#include "RecoMuon/MeasurementDet/interface/MuonDetLayerMeasurements.h"
+#include "RecoMuon/DetLayers/interface/MuonDetLayerGeometry.h"
+#include "RecoMuon/Records/interface/MuonRecoGeometryRecord.h"
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
 
 #include <vector>
 
@@ -44,18 +57,22 @@ public:
   static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
   void produce(edm::Event&, const edm::EventSetup&) override;
 
-  /// Cache Magnetic Field for current event
-  void setBField(const MagneticField* theField) { BField = theField; };
-
   /// Create a seed from set of segments
-  L2MuonTrajectorySeed createSeed(int type,
-                                  const SegmentContainer& seg,
-                                  const l1t::TrackerMuon& l1TkMu,
-                                  double const& dRCone);
+  L2MuonTrajectorySeed createSeed(CSCSegmentCollection cscSegments,
+                                  DTRecSegment4DCollection dtSegments,
+                                  l1t::TrackerMuonRef l1TkMuRef,
+                                  const double& dRCone);
 
 private:
-  edm::InputTag source_;
-  edm::EDGetTokenT<l1t::TrackerMuonCollection> muCollToken_;
+  // Tokens
+  edm::EDGetTokenT<l1t::TrackerMuonCollection> l1TkMuCollToken_;
+  edm::EDGetTokenT<CSCSegmentCollection> cscSegmentCollToken_;
+  edm::EDGetTokenT<DTRecSegment4DCollection> dtSegmentCollToken_;
+
+  edm::ESGetToken<CSCGeometry, MuonGeometryRecord> cscGeometryToken_;
+  edm::ESGetToken<DTGeometry, MuonGeometryRecord> dtGeometryToken_;
+  edm::ESGetToken<MagneticField, IdealMagneticFieldRecord> magneticFieldToken_;
+  edm::ESGetToken<MuonDetLayerGeometry, MuonRecoGeometryRecord> muonLayersToken_;
 
   // Miminum and maximum pt momentum of a track
   double minMomentum_;
@@ -63,13 +80,16 @@ private:
   double defaultMomentum_;
 
   // Flag for internal debugging
-  bool debug;
+  bool debug_;
 
   // Error on pt estimate which prevents weighted average from blowing up ( spt --> 0 )
-  double sysError;
+  double sysError_;
 
-  // Cache Magnetic Field for current event
-  const MagneticField* BField;
+  // Handles
+  edm::ESHandle<MagneticField> magneticField_;
+  edm::ESHandle<MuonDetLayerGeometry> muonLayers_;
+  edm::ESHandle<CSCGeometry> cscGeometry_;
+  edm::ESHandle<DTGeometry> dtGeometry_;
 
   // seed parameters vectors
   std::vector<double> DT12;
