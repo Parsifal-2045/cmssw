c78f1f49c3243812f5d80c5ecbfbab14f674b362
Luca Ferragina <lferragi@patatrack02.cern.ch>
2024-05-13 15:17:59 +0200

    Add Phase2 L2 Muons sequence and modules

---
+++ HLTrigger/Configuration/python/HLT_75e33/modules/Phase2L2MuonSeedCreator_cfi.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+Phase2L2MuonSeedCreator = cms.EDProducer('Phase2L2MuonSeedCreator',
+  InputObjects = cms.InputTag('hltGmtStage2Digis'),
+  CSCRecSegmentLabel = cms.InputTag('hltCscSegments'),
+  DTRecSegmentLabel = cms.InputTag('hltDt4DSegments'),
+  MinPL1Tk = cms.double(3.5),
+  MaxPL1Tk = cms.double(200),
+  StubMatchDR = cms.double(0.25),
+  maximumEtaBarrel = cms.double(0.7),
+  maximumEtaOverlap = cms.double(1.3),
+  mightGet = cms.optional.untracked.vstring
+)
