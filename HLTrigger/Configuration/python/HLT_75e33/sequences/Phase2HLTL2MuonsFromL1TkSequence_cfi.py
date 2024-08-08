e7a6972641870ed2c0e21d9fae5972815d59c3b4
Luca <ferragina.luca@gmail.com>
2024-08-08 18:10:47 +0200

    Format all Phase2 HLT Muons Sequences to improve readability

--- HLTrigger/Configuration/python/HLT_75e33/sequences/Phase2HLTL2MuonsFromL1TkSequence_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/sequences/Phase2HLTL2MuonsFromL1TkSequence_cfi.py
@@ -4,4 +4,6 @@ from ..sequences.muonlocalrecoSequence_cfi import *
 from ..modules.hltL2MuonSeedsFromL1TkMuon_cfi import * # includes PHASE2_TAG
 from ..modules.hltL2MuonsFromL1TkMuon_cfi import *
 
-Phase2HLTL2MuonsFromL1TkSequence = cms.Sequence(muonlocalrecoSequence+hltL2MuonSeedsFromL1TkMuon+hltL2MuonsFromL1TkMuon)
+Phase2HLTL2MuonsFromL1TkSequence = cms.Sequence(muonlocalrecoSequence
+                                                +hltL2MuonSeedsFromL1TkMuon
+                                                +hltL2MuonsFromL1TkMuon)
