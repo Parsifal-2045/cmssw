07a4873baf5d3d40b9fba4a49ec631ee59a3ab25
Luca Ferragina <ferragina.luca@gmail.com>
2025-06-20 10:18:00 +0200

    Refactor HLT Phase 2 Muons Sequence
    Rearrange Tracker Muons (Inside-Out and Outside-In) and Muon ID sequences
    with clearer naming and separation

--- HLTrigger/Configuration/python/HLT_75e33/sequences/HLTMuonsSequence_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/sequences/HLTMuonsSequence_cfi.py
@@ -1,33 +1,27 @@
 import FWCore.ParameterSet.Config as cms
 
-from ..sequences.HLTIter0Phase2L3FromL1TkSequence_cfi import *
-from ..sequences.HLTIter2Phase2L3FromL1TkSequence_cfi import *
 from ..sequences.HLTL2MuonsFromL1TkSequence_cfi import *
-from ..sequences.HLTPhase2L3FromL1TkSequence_cfi import *
-from ..sequences.HLTPhase2L3MuonsSequence_cfi import *
-from ..sequences.HLTPhase2L3OISequence_cfi import *
+from ..sequences.HLTPhase2L3MuonsIOSequence_cfi import *
+from ..sequences.HLTPhase2L3MuonsOISequence_cfi import *
+from ..sequences.HLTPhase2MuonIdSequence_cfi import *
 from ..modules.hltPhase2L3MuonFilter_cfi import *
 
 # The default HLT Muons sequence (Inside-Out first)
 HLTMuonsSequence = cms.Sequence(
     HLTL2MuonsFromL1TkSequence
-    + HLTPhase2L3FromL1TkSequence
-    + HLTIter0Phase2L3FromL1TkSequence
-    + HLTIter2Phase2L3FromL1TkSequence
+    + HLTPhase2L3MuonsIOSequence
     + hltPhase2L3MuonFilter
-    + HLTPhase2L3OISequence
-    + HLTPhase2L3MuonsSequence
+    + HLTPhase2L3MuonsOISequence
+    + HLTPhase2MuonIdSequence
 )
 
 # Outside-In first HLT Muons sequence
 _HLTMuonsSequenceOIFirst = cms.Sequence(
     HLTL2MuonsFromL1TkSequence
-    + HLTPhase2L3OISequence
+    + HLTPhase2L3MuonsOISequence
     + hltPhase2L3MuonFilter
-    + HLTPhase2L3FromL1TkSequence
-    + HLTIter0Phase2L3FromL1TkSequence
-    + HLTIter2Phase2L3FromL1TkSequence
-    + HLTPhase2L3MuonsSequence
+    + HLTPhase2L3MuonsIOSequence
+    + HLTPhase2MuonIdSequence
 )
 
 from Configuration.ProcessModifiers.phase2L3MuonsOIFirst_cff import phase2L3MuonsOIFirst
