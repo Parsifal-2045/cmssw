c44a11b9834448b82bc8ab5c39aafb72296fa35c
Luca Ferragina <ferragina.luca@gmail.com>
2025-07-01 17:44:27 +0200

    Optimize HighPurity selection for iter0

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltIter0Phase2L3FromL1TkMuonTrackCutClassifier_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltIter0Phase2L3FromL1TkMuonTrackCutClassifier_cfi.py
@@ -33,9 +33,35 @@ hltIter0Phase2L3FromL1TkMuonTrackCutClassifier = cms.EDProducer("TrackCutClassif
     vertices = cms.InputTag("hltPhase2L3FromL1TkMuonTrimmedPixelVertices")
 )
 
-from Configuration.ProcessModifiers.alpaka_cff import alpaka
-alpaka.toModify(
+from Configuration.ProcessModifiers.phase2CAExtension_cff import phase2CAExtension
+phase2CAExtension.toModify(
     hltIter0Phase2L3FromL1TkMuonTrackCutClassifier,
-    vertices = "hltPhase2PixelVertices"
+    ignoreVertices = True,
+    mva = dict(
+        dr_par = dict(
+            d0err = [1.610380103264296148, 0.502722713333466853, 1.111607948895815268],
+            d0err_par = [64.742912303519062789, 81.012436524573445240, 54.385119178719129707],
+            dr_exp = [19, 10, 13],
+            dr_par1 = [57.059481459985178731, 50.279776876120820361, 33.988307002383727706],
+            dr_par2 = [74.068635810153196530, 34.471315687253465398, 45.226909827846640155]
+        ),
+        dz_par = dict(
+            dz_exp = [10, 6, 9],
+            dz_par1 = [45.512594312811913255, 53.117027806739699258, 82.032029513077333149],
+            dz_par2 = [20.052051082517227343, 26.960171536570406658, 74.717977412227568834]
+        ), 
+    maxChi2 = [75.613179075702618093, 20.149173982721297449, 24.655685312520887464],
+    maxChi2n = [75.613179075702618093, 20.149173982721297449, 24.655685312520887464],
+    maxDr = [0.632564511692216502, 0.899389163528369062, 1.021550664333019887],
+    maxDz = [55.531736492650743742, 4.605282832364866685, 35.255653778578881941],
+    maxDzWrtBS = [65.655403829487170242, 62.902571518998129818, 71.481381340634541743],
+    maxLostLayers = [6, 7, 4],
+    min3DLayers = [4, 4, 4],
+    minLayers = [3, 5, 8],
+    minNVtxTrk = 5,
+    minNdof = [0.789722702402609444, 1.496971638145484285, 1.666093571637450532],
+    minPixelHits = [5, 1, 1]
+    ),
+    qualityCuts = [-0.023233126663624204, 0.132977980206018087, 0.490682422036849974],
+    vertices = ""
 )
-
