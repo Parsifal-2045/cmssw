7ee7d75e2c4094192b133c7f5a2735fe63b7e353
Luca Ferragina <ferragina.luca@gmail.com>
2026-02-06 14:55:57 +0100

    Implement DNN for Outside-In Muon tracks High-Purity selection

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2L3OIMuonTrackSelectionHighPurity_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2L3OIMuonTrackSelectionHighPurity_cfi.py
@@ -8,3 +8,27 @@ hltPhase2L3OIMuonTrackSelectionHighPurity = cms.EDProducer("TrackCollectionFilte
     originalQualVals = cms.InputTag("hltPhase2L3OIMuonTrackCutClassifier","QualityMasks"),
     originalSource = cms.InputTag("hltPhase2L3OIMuCtfWithMaterialTracks")
 )
+
+_hltPhase2L3OIMuonTrackSelectionHighPurityPixelSelector = cms.EDProducer("MuonOITracksDNNSelector",
+    decisionThreshold = cms.double(0.9775444269180298),
+    nFeatures = cms.int32(26),
+    modelPath = cms.string("RecoMuon/L3TrackFinder/data/OI_pixel_model.onnx")
+)
+
+_hltPhase2L3OIMuonTrackSelectionHighPurityGeneralSelector = cms.EDProducer("MuonOITracksDNNSelector",
+    decisionThreshold = cms.double(0.9548693895339966),
+    nFeatures = cms.int32(26),
+    modelPath = cms.string("RecoMuon/L3TrackFinder/data/OI_general_model.onnx")
+)
+
+from Configuration.ProcessModifiers.phase2CAExtension_cff import phase2CAExtension
+phase2CAExtension.toReplaceWith(
+    hltPhase2L3OIMuonTrackSelectionHighPurity,
+    _hltPhase2L3OIMuonTrackSelectionHighPurityPixelSelector
+)
+
+from Configuration.ProcessModifiers.trackingLST_cff import trackingLST
+(phase2CAExtension & trackingLST).toReplaceWith(
+    hltPhase2L3OIMuonTrackSelectionHighPurity,
+    _hltPhase2L3OIMuonTrackSelectionHighPurityGeneralSelector
+)
