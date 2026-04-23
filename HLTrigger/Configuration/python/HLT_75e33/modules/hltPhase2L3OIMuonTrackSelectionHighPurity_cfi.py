abba147278b59e863e4a0b7f51f51e850007d6ee
Luca Ferragina <ferragina.luca@gmail.com>
2026-04-23 10:36:34 +0200

    Switch to DNN-based High-Purity selection for OI Muon tracks
    The MVA-based High-Purity selection can be optionally replaced with a
    DNN approach that follows the implementation for IO selection

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2L3OIMuonTrackSelectionHighPurity_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2L3OIMuonTrackSelectionHighPurity_cfi.py
@@ -8,3 +8,30 @@ hltPhase2L3OIMuonTrackSelectionHighPurity = cms.EDProducer("TrackCollectionFilte
     originalQualVals = cms.InputTag("hltPhase2L3OIMuonTrackCutClassifier","QualityMasks"),
     originalSource = cms.InputTag("hltPhase2L3OIMuCtfWithMaterialTracks")
 )
+
+_pixelDNNSelector = cms.EDProducer('MuonOITracksDNNSelector',
+    tracks = cms.InputTag('hltPhase2L3OIMuCtfWithMaterialTracks'),
+    standaloneMuons = cms.InputTag('hltL2MuonsFromL1TkMuon', 'UpdatedAtVtx'),
+    modelPath = cms.string('RecoMuon/L3TrackFinder/data/OI_pixel_model.onnx'),
+    decisionThreshold = cms.double(0.9775444269180298),
+    useStandaloneMuonFeatures = cms.bool(True),
+    nFeatures = cms.int32(26),
+)
+
+_seedsDNNSelector = _pixelDNNSelector.clone(
+    decisionThreshold = cms.double(0.9548693895339966),
+    modelPath = cms.string("RecoMuon/L3TrackFinder/data/OI_general_model.onnx")
+)
+
+from Configuration.ProcessModifiers.phase2MuonPixelTracksSelector_cff import phase2MuonPixelTracksSelector
+from Configuration.ProcessModifiers.ngtScouting_cff import ngtScouting
+(phase2MuonPixelTracksSelector | ngtScouting).toReplaceWith(
+    hltPhase2L3OIMuonTrackSelectionHighPurity,
+    _pixelDNNSelector
+)
+
+from Configuration.ProcessModifiers.phase2MuonSeedsSelector_cff import phase2MuonSeedsSelector
+(phase2MuonSeedsSelector).toReplaceWith(
+    hltPhase2L3OIMuonTrackSelectionHighPurity,
+    _seedsDNNSelector
+)
